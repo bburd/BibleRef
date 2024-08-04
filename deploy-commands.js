bbd088cee629c319105515c91d253658ca3cf0fc
bburd <51426310+bburd@users.noreply.github.com>
2024-08-03 23:23:39 -0400

    Remade database and dependent scripts

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,15 +1,10 @@
 // Please forgive me for this awful code...
 // It is my first project, courtesy of ChatGPT 4o. -bburd
 
+require("dotenv").config();
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const fs = require("fs");
-const path = require("path");
-
-// Load configuration
-const config = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "config.json"), "utf8")
-);
 
 const commands = [];
 const commandFiles = fs
@@ -21,22 +16,25 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: "9" }).setToken(config.token);
+const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
     console.log("Started refreshing application (/) commands.");
 
-    if (config.guildId) {
+    if (process.env.GUILD_ID) {
       await rest.put(
-        Routes.applicationGuildCommands(config.clientId, config.guildId),
+        Routes.applicationGuildCommands(
+          process.env.CLIENT_ID,
+          process.env.GUILD_ID
+        ),
         { body: commands }
       );
       console.log(
         "Successfully reloaded guild-specific application (/) commands."
       );
     } else {
-      await rest.put(Routes.applicationCommands(config.clientId), {
+      await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
         body: commands,
       });
       console.log("Successfully reloaded global application (/) commands.");
