ea26b76d53a8280ca046787a45fdca61c1ba8c83
bburd <51426310+bburd@users.noreply.github.com>
2025-08-31 22:01:38 -0400

    Revert "Add shared Strong's stripping helper and optional translation cleanup" (#93)
    Revert "Add shared Strong's stripping helper and optional translation cleanup…"
    
    This reverts commit 69a7c435c714ed042d4f56f75519ec0da0ea645b.

--- src/db/stripStrongs.js
+++
@@ -1,8 +0,0 @@
-const STRONGS_REGEX = /\{[GH]\s*\d{1,5}\}|\[[GH]\s*\d{1,5}\]|<\s*[GH]\s*\d{1,5}\s*>/g;
-
-function stripStrongs(text = '') {
-  if (!text) return text;
-  return text.replace(STRONGS_REGEX, '').replace(/\s+/g, ' ').trim();
-}
-
-module.exports = { STRONGS_REGEX, stripStrongs };
